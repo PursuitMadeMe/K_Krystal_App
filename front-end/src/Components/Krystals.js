f02976d8e0cde0b72d2a2f0c021763caa88cccd9
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-09-01 01:13:19 -0400

    select menu

--- front-end/src/Components/Krystals.js
+++ front-end/src/Components/Krystals.js
@@ -14,9 +14,61 @@ function Krystals() {
   //invoking useState and setting the variable snack to an object and setSnack which updates the component
   const [krystals, setKrystals] = useState([]);
 
+  const healingProperties = [
+    {
+      label: 'Anxiety',
+      value: 'anxiety',
+  
+    },
+    {
+      label: 'Motivation',
+      value: 'motivation',
+  
+    },
+    {
+      label: 'Study',
+      value: 'study',
+  
+    },
+    {
+      label: 'Love',
+      value: 'love',
+  
+    },
+    {
+      label: 'Stress',
+      value: 'stress',
+  
+    },
+    {
+      label: 'Happiness',
+      value: 'happiness',
+  
+    },
+    {
+      label: 'Prosperity',
+      value: 'prosperity',
+  
+    },
+    {
+      label: 'Manifestation',
+      value: 'manifestation',
+  
+    }
+  ]
+
+  const [healing, setHealing] = useState("Select a Healing Property");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setHealing(e.target.value)
+  };
+
+
   //useEffect is invoked and within the it we have 2 parameters our axios that fetches our API and array of dependencies
   //then we are updating the variable using setSnack SETTING it equal to res.data
   //adding a catch to notify when an error occurs
+
   useEffect(() => {
     axios
       .get(`${API}/krystals`)
@@ -25,9 +77,21 @@ function Krystals() {
   }, []);
   console.log(krystals);
 
+
+
   return (
     <div>
       <section className='krystals-index'>
+      <br/>
+      <select onChange={handleChange}>
+      <br/>
+      <br/>
+
+        <option value="Select A Healing Property"> -- Select A Healing Property </option>
+        {healingProperties.map((prop) => <option value={prop.value}>{prop.label}</option>)}
+        </select>
+        
+
         {krystals.map((krystal, index) => {
           return <Krystal krystal={krystal} index={index} />;
         })}
--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,53 +1,12 @@
 import React from 'react'
 
 function Home() {
-    const options = [
-        {
-          label: 'Anxiety',
-          value: 'anxiety',
-      
-        },
-        {
-          label: 'Motivation',
-          value: 'motivation',
-      
-        },
-        {
-          label: 'Study',
-          value: 'study',
-      
-        },
-        {
-          label: 'Love',
-          value: 'love',
-      
-        },
-        {
-          label: 'Stress',
-          value: 'stress',
-      
-        },
-        {
-          label: 'Happiness',
-          value: 'happiness',
-      
-        },
-        {
-          label: 'Prosperity',
-          value: 'prosperity',
-      
-        },
-        {
-          label: 'Manifestation',
-          value: 'manifestation',
-      
-        }
-    ]
+   
   return (
     <div>
         <h1>Kim's Crystal Healing App</h1>
-        <h2> today?</h2>
-        <select><options>What do you need help with?</options></select>
+        <h2> today?</h2>What do you need help with?
+        <select><options></options></select>
     </div>
   )
 }
