2f1b029d41c39b85a77b2f48716818cb8cc2084f
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2023-01-17 16:21:06 -0500

    seed and schema for healers and chakras table with healersController and queries

---
+++ controllers/healersController.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const rewards = express.Router();
+const {
+  getAllHealers,
+  getHeal,
+  createHeal,
+  updateHeal,
+  deleteHeal,
+} = require("../queries/healers.js");
+
+//INDEX
+healers.get("/", async (req, res) => {
+  const allHealers = await getAllHealers();
+  if (allHealers[0]) {
+    res.status(200).json({ payload: allHealers, success: true });
+  } else {
+    res.status(500).json({ error: "server error line 17 healersController" });
+  }
+});
+
+// Read (Singular/Specific ID)
+healers.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const heal = await getHeal(id);
+  if (heal.id) {
+    res.json({ payload: heal, success: true });
+  } else {
+    res.status(404).json({
+      payload: "not found",
+      success: false,
+      error: "heal not found line 31 healersController",
+    });
+  }
+});
+
+// Create
+healers.post("/", async (req, res) => {
+  const { body } = req;
+
+  body.id = body;
+
+  try {
+    const createdHealer = await createHeal(body.payload);
+    if (createdHealer.id) {
+      res.status(200).json({
+        success: true,
+        payload: createdHealer,
+      });
+    } else {
+      res.status(422).json({
+        success: false,
+        payload: "Must include type field line 52 healersController",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Update
+healers.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+
+  body.id = body;
+
+  const updatedHeal = await updateHeal(req.body, id);
+  console.log(updatedHeal);
+  if (updatedHeal.id) {
+    res.status(200).json(updatedHeal);
+  } else {
+    res.status(404).json({ error: "heal not found line 72 healersController" });
+  }
+});
+
+// Delete
+healers.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedHeal = await deleteHeal(id);
+  if (deletedHeal.id) {
+    res.status(200).json({ payload: deletedHeal, success: true });
+  } else {
+    res.status(404).json({ payload: "heal not found line 83 healersCoontroller", success: false });
+  }
+});
+
+module.exports = healers;
--- db/prod_schema.sql
+++ db/prod_schema.sql
@@ -1,7 +1,4 @@
-DROP DATABASE IF EXISTS test;
-
-
-CREATE TABLE test (
+CREATE TABLE krystals (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     healing TEXT NOT NULL,
@@ -11,3 +8,45 @@ CREATE TABLE test (
     category TEXT,
     is_favorite BOOLEAN
 );
+
+ DROP TABLE IF EXISTS users;
+
+--  CREATE TABLE users(
+--     user_id TEXT PRIMARY KEY UNIQUE NOT NULL, 
+--     email TEXT NOT NULL,
+--     username TEXT NOT NULL,
+--     firsname TEXT NOT NULL,
+--     lastname TEXT NOT NULL
+--  );   
+
+DROP TABLE IF EXISTS healears;
+
+ CREATE TABLE healears(
+    id SERIAL PRIMARY KEY, 
+    feelings TEXT NOT NULL
+ );   
+
+DROP TABLE IF EXISTS chakras;
+
+ CREATE TABLE chakras(
+    id SERIAL PRIMARY KEY, 
+    type TEXT NOT NULL,
+    attribute TEXT NOT NULL
+ );  
+
+-- DROP TABLE IF EXISTS users_healers;
+
+-- CREATE TABLE users_healers (
+--     user_id TEXT NOT NULL,
+--     healer_id INTEGER NOT NULL,
+--     created TIMESTAMP DEFAULT NOW()
+-- );
+
+-- DROP TABLE IF EXISTS users_chakras;
+
+-- CREATE TABLE users_chakras (
+--     user_id TEXT NOT NULL,
+--     chakra_id INTEGER NOT NULL,
+--     created TIMESTAMP DEFAULT NOW()
+-- );
+
