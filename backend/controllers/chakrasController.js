acda75cf8e765d39797e681ae919b28c9f32b0da
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2024-05-19 21:53:16 -0400

    try_catch

--- backend/controllers/chakrasController.js
+++ backend/controllers/chakrasController.js
@@ -9,25 +9,31 @@ const {
 } = require("../queries/chakras.js");
 
 chakras.get("/", async (req, res) => {
-  const allChakras = await getAllChakras();
-  if (allChakras[0]) {
-    res.status(200).json({ payload: allChakras, success: true });
-  } else {
-    res.status(500).json({ error: "server error line 17 chakrasController" });
+  try {
+    const allChakras = await getAllChakras();
+    if (allChakras[0]) {
+      res.status(200).json({ payload: allChakras, success: true });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
 chakras.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const chakra = await getChakra(id);
-  if (chakra.id) {
-    res.json({ payload: chakra, success: true });
-  } else {
-    res.status(404).json({
-      payload: "not found",
-      success: false,
-      error: "chakra not found line 31 chakrasController",
-    });
+  try {
+    const { id } = req.params;
+    const chakra = await getChakra(id);
+    if (chakra.id) {
+      res.json({ payload: chakra, success: true });
+    } else {
+      res.status(404).json({
+        payload: "not found",
+        success: false,
+        error: "chakra not found line 31 chakrasController",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
@@ -55,34 +61,40 @@ chakras.post("/", async (req, res) => {
 });
 
 chakras.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { body } = req;
+  try {
+    const { id } = req.params;
+    const { body } = req;
 
-  body.id = body;
+    body.id = body;
 
-  const updatedChakra = await updateChakra(req.body, id);
-  console.log(updatedChakra);
-  if (updatedChakra.id) {
-    res.status(200).json(updatedChakra);
-  } else {
-    res
-      .status(404)
-      .json({ error: "chakra not found line 72 chakrasController" });
+    const updatedChakra = await updateChakra(req.body, id);
+    console.log(updatedChakra);
+    if (updatedChakra.id) {
+      res.status(200).json(updatedChakra);
+    } else {
+      res
+        .status(404)
+        .json({ error: "chakra not found line 72 chakrasController" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
 chakras.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  const deletedChakra = await deleteChakra(id);
-  if (deletedChakra.id) {
-    res.status(200).json({ payload: deletedChakra, success: true });
-  } else {
-    res
-      .status(404)
-      .json({
+  try {
+    const { id } = req.params;
+    const deletedChakra = await deleteChakra(id);
+    if (deletedChakra.id) {
+      res.status(200).json({ payload: deletedChakra, success: true });
+    } else {
+      res.status(404).json({
         payload: "chakra not found line 83 chakrasCoontroller",
         success: false,
       });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
