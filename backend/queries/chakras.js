8a1fee5ed486b7b9d90e68f4981206a7950a9734
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2024-05-30 11:06:20 -0400

    Remove Try/Catch, add db.oneOrNone search parameter to chakra Queries

--- backend/queries/chakras.js
+++ backend/queries/chakras.js
@@ -1,60 +1,41 @@
 const db = require("../db/dbConfig.js");
 
 const getAllChakras = async () => {
-  try {
-    const allChakras = await db.any("SELECT * FROM chakras");
-    return allChakras;
-  } catch (err) {
-    return err;
-  }
+  const allChakras = await db.any("SELECT * FROM chakras");
+  return allChakras;
 };
 
 const getChakra = async (id) => {
-  try {
-    const chakra = await db.one("SELECT * FROM chakras WHERE id=$1", id);
-    //change return from heal to chakra
-    return chakra;
-  } catch (err) {
-    return err;
-  }
+  const chakra = await db.oneOrNone("SELECT * FROM chakras WHERE id=$1", id);
+  //change return from heal to chakra
+  return chakra;
 };
 
 const createChakra = async (chakra) => {
   const { id, type, attribute } = chakra;
-  try {
-    const createdChakra = await db.one(
-      "INSERT INTO chakras (id, type, attribute) VALUES ($1, $2, $3) RETURNING *",
-      [id, type, attribute]
-    );
-    return createdChakra;
-  } catch (err) {
-    return err;
-  }
+
+  const createdChakra = await db.oneOrNone(
+    "INSERT INTO chakras (id, type, attribute) VALUES ($1, $2, $3) RETURNING *",
+    [id, type, attribute]
+  );
+  return createdChakra;
 };
 
 const updateChakra = async (id, chakra) => {
-  const {type, attribute } = chakra;
-  try {
-    const updatedChakra = await db.one(
-      "UPDATE chakras SET type=$1, attribute=$2 WHERE id=$3 RETURNING *",
-      [type, attribute, id]
-    );
-    return updatedChakra;
-  } catch (err) {
-    return err;
-  }
+  const { type, attribute } = chakra;
+  const updatedChakra = await db.oneOrNone(
+    "UPDATE chakras SET type=$1, attribute=$2 WHERE id=$3 RETURNING *",
+    [type, attribute, id]
+  );
+  return updatedChakra;
 };
 
 const deleteChakra = async (id) => {
-  try {
-    const deletedChakra = await db.one(
-      "DELETE FROM chakras WHERE id=$1 RETURNING *",
-      id
-    );
-    return deletedChakra;
-  } catch (err) {
-    return err;
-  }
+  const deletedChakra = await db.oneOrNone(
+    "DELETE FROM chakras WHERE id=$1 RETURNING *",
+    id
+  );
+  return deletedChakra;
 };
 
 module.exports = {
