4785e388181b56611e32ea97f2679f3c8799771e
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2024-06-16 23:42:15 -0400

    Remove Try/Catch and Add oneOrNone in queries

--- backend/queries/krystals.js
+++ backend/queries/krystals.js
@@ -2,41 +2,34 @@ const db = require("../db/dbConfig.js");
 
 // INDEX Route - DONE 
 const getAllKrystals = async () => {
-    try {
+    
         const allKrystals = await db.any("SELECT * FROM krystals");
         return allKrystals;
-      } catch (error) {
-        return error;
-      }
+
     };
 
     // SHOW Route - DONE 
     const getKrystal = async (id) => {
-        try {
+        
           const oneKrystal = await db.oneOrNone("SELECT * FROM krystals WHERE id=$1", id);
           return oneKrystal;
-        } catch (error) {
-          return error;
-        }
+       
       };
 
       // CREATE Route - DONE
 const createKrystal = async (krystal) => {
-    try {
+   
       const newKrystal = await db.oneOrNone(
         "INSERT INTO krystals (name, healing, chakra, image, numerology, category, is_favorite) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
         [krystal.name, krystal.healing,krystal.chakra, krystal.image, krystal.numerology, krystal.category, krystal.is_favorite]
       );
       console.log("NEW CRYSTAL : ",newKrystal)
       return newKrystal;
-    } catch (error) {
-      return error;
-    }
+   
   };
 
   // UPDATE Route - DONE
 const updateKrystal = async (krystal, id) => {
-    try {
       const updatedKrystal = await db.oneOrNone(
         "UPDATE krystals SET name=$1, healing=$2, chakra=$3, image=$4, numerology=$5, category=$6, is_favorite=$7  WHERE id=$8 RETURNING *",
         [
@@ -52,23 +45,16 @@ const updateKrystal = async (krystal, id) => {
       );
       console.log(updatedKrystal, "GOOD VIBES");
       return updatedKrystal;
-    } catch (error) {
-      console.log("NO UPDATE");
   
-      return error;
-    }
   };
   // DELETE Route - DONE
   const deleteKrystal = async (id) => {
-    try {
       const deletedKrystal = await db.oneOrNone(
         "DELETE FROM krystals WHERE id=$1 RETURNING *",
         id
       );
       return deletedKrystal;
-    } catch (error) {
-      return error;
-    }
+   
   };
   
 
