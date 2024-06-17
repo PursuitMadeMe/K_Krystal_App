afa921c8e8e0941f581af7d6ec268e35033e5eb6
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2024-06-16 23:48:53 -0400

    Remove Try/catch Add oneOrNone to queries

--- backend/queries/healers.js
+++ backend/queries/healers.js
@@ -1,59 +1,40 @@
 const db = require("../db/dbConfig.js");
 
 const getAllHealers = async () => {
-  try {
-    const allHealers = await db.any("SELECT * FROM healers");
-    return allRewards;
-  } catch (err) {
-    return err;
-  }
+  const allHealers = await db.any("SELECT * FROM healers");
+  return allRewards;
 };
 
 const getHeal = async (id) => {
-  try {
-    const heal = await db.one("SELECT * FROM healers WHERE id=$1", id);
-    return heal;
-  } catch (err) {
-    return err;
-  }
+  const heal = await db.oneOrNone("SELECT * FROM healers WHERE id=$1", id);
+  return heal;
 };
 
 const createHeal = async (heal) => {
   const { id, feelings } = heal;
-  try {
-    const createdHeal = await db.one(
-      "INSERT INTO healers (id, feelings) VALUES ($1, $2) RETURNING *",
-      [id, feelings]
-    );
-    return createdHeal;
-  } catch (err) {
-    return err;
-  }
+
+  const createdHeal = await db.oneOrNone(
+    "INSERT INTO healers (id, feelings) VALUES ($1, $2) RETURNING *",
+    [id, feelings]
+  );
+  return createdHeal;
 };
 
 const updateHeal = async (id, heal) => {
-  const {feelings } = heal;
-  try {
-    const updatedHeal = await db.one(
-      "UPDATE healers SET feelings=$1 WHERE id=$2 RETURNING *",
-      [feelings, id]
-    );
-    return updatedHeal;
-  } catch (err) {
-    return err;
-  }
+  const { feelings } = heal;
+  const updatedHeal = await db.oneOrNone(
+    "UPDATE healers SET feelings=$1 WHERE id=$2 RETURNING *",
+    [feelings, id]
+  );
+  return updatedHeal;
 };
 
 const deleteHeal = async (id) => {
-  try {
-    const deletedHeal = await db.one(
-      "DELETE FROM healers WHERE id =$1 RETURNING *",
-      id
-    );
-    return deletedHeal;
-  } catch (err) {
-    return err;
-  }
+  const deletedHeal = await db.oneOrNone(
+    "DELETE FROM healers WHERE id =$1 RETURNING *",
+    id
+  );
+  return deletedHeal;
 };
 
 module.exports = {
--- backend/queries/krystals.js
+++ backend/queries/krystals.js
@@ -1,61 +1,69 @@
 const db = require("../db/dbConfig.js");
 
-// INDEX Route - DONE 
+// INDEX Route - DONE
 const getAllKrystals = async () => {
-    
-        const allKrystals = await db.any("SELECT * FROM krystals");
-        return allKrystals;
+  const allKrystals = await db.any("SELECT * FROM krystals");
+  return allKrystals;
+};
 
-    };
+// SHOW Route - DONE
+const getKrystal = async (id) => {
+  const oneKrystal = await db.oneOrNone(
+    "SELECT * FROM krystals WHERE id=$1",
+    id
+  );
+  return oneKrystal;
+};
 
-    // SHOW Route - DONE 
-    const getKrystal = async (id) => {
-        
-          const oneKrystal = await db.oneOrNone("SELECT * FROM krystals WHERE id=$1", id);
-          return oneKrystal;
-       
-      };
-
-      // CREATE Route - DONE
+// CREATE Route - DONE
 const createKrystal = async (krystal) => {
-   
-      const newKrystal = await db.oneOrNone(
-        "INSERT INTO krystals (name, healing, chakra, image, numerology, category, is_favorite) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
-        [krystal.name, krystal.healing,krystal.chakra, krystal.image, krystal.numerology, krystal.category, krystal.is_favorite]
-      );
-      console.log("NEW CRYSTAL : ",newKrystal)
-      return newKrystal;
-   
-  };
+  const newKrystal = await db.oneOrNone(
+    "INSERT INTO krystals (name, healing, chakra, image, numerology, category, is_favorite) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+    [
+      krystal.name,
+      krystal.healing,
+      krystal.chakra,
+      krystal.image,
+      krystal.numerology,
+      krystal.category,
+      krystal.is_favorite,
+    ]
+  );
+  console.log("NEW CRYSTAL : ", newKrystal);
+  return newKrystal;
+};
 
-  // UPDATE Route - DONE
+// UPDATE Route - DONE
 const updateKrystal = async (krystal, id) => {
-      const updatedKrystal = await db.oneOrNone(
-        "UPDATE krystals SET name=$1, healing=$2, chakra=$3, image=$4, numerology=$5, category=$6, is_favorite=$7  WHERE id=$8 RETURNING *",
-        [
-          krystal.name,
-          krystal.healing,
-          krystal.chakra,
-          krystal.image,
-          krystal.numerology,
-          krystal.category,
-          krystal.is_favorite,
-          id,
-        ]
-      );
-      console.log(updatedKrystal, "GOOD VIBES");
-      return updatedKrystal;
-  
-  };
-  // DELETE Route - DONE
-  const deleteKrystal = async (id) => {
-      const deletedKrystal = await db.oneOrNone(
-        "DELETE FROM krystals WHERE id=$1 RETURNING *",
-        id
-      );
-      return deletedKrystal;
-   
-  };
-  
+  const updatedKrystal = await db.oneOrNone(
+    "UPDATE krystals SET name=$1, healing=$2, chakra=$3, image=$4, numerology=$5, category=$6, is_favorite=$7  WHERE id=$8 RETURNING *",
+    [
+      krystal.name,
+      krystal.healing,
+      krystal.chakra,
+      krystal.image,
+      krystal.numerology,
+      krystal.category,
+      krystal.is_favorite,
+      id,
+    ]
+  );
+  console.log(updatedKrystal, "GOOD VIBES");
+  return updatedKrystal;
+};
+// DELETE Route - DONE
+const deleteKrystal = async (id) => {
+  const deletedKrystal = await db.oneOrNone(
+    "DELETE FROM krystals WHERE id=$1 RETURNING *",
+    id
+  );
+  return deletedKrystal;
+};
 
-module.exports = { getAllKrystals, getKrystal, createKrystal, updateKrystal, deleteKrystal };
+module.exports = {
+  getAllKrystals,
+  getKrystal,
+  createKrystal,
+  updateKrystal,
+  deleteKrystal,
+};
